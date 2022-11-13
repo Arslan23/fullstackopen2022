0ed570add041973d79071a3b34cfc3e4df3fbe06
Arslan23 <expeditaza@gmail.com>
2022-11-13 18:14:45 +0100

    Exercices 1.6.-1.14

--- README.md
+++ README.md
@@ -76,3 +76,6 @@ In the previous sections of the course we used MongoDB for storing data, which i
 Relational databases have since then experienced a new beginning. Problems with scalability have been partially resolved and they have also adopted some of the features of NoSQL databases. In this section we explore different NodeJS applications that use relational databases, we will focus on using the database PostgreSQL which is the number one in the open source world.
 
 
+<p>{anecdotes.at(vote.indexOf(vote.max))}
+      <br/> has {Math.max(...vote)} votes
+      </p>
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,70 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Header = ()=>{ return (<h1>give feedback</h1>)}
+const Header = () => {
+  return <h1> give feedback </h1>;
+};
 
-const Statistics = ({good, neutral, bad})=>{
-  const all = good + neutral + bad
-  const average  = (good - bad) / all
-  const positive = good / all
-  
-  return (
-<div>
-<h1>statistics</h1>
-{ 
-all === 0 ? (
-  <p>
-  <span>good: {good}</span>
-  <br/>
-  <span>neutral: {neutral}</span>
-  <br/>
-  <span>bad: {bad}</span>
-  <br/>
-  <span>all: {all}</span>
-  <br/>
-  <span>average: {average}</span>
-  <br/>
-  <span>positive: {positive}</span>
-</p>
-) : (
-<p>
-No feedback given
-</p>
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100 + ' %';
+  if(all === 0)
+  {
+    return (<div>
+    <h1>
+        statistics
+        </h1>
+    <p>
+         No feedback given
+    </p>
 
-)}
-
-</div>
-
-)}
-
-/*
-const Button = ({text, clicked })=>{
-
-
-  const incrementGood = ()=>{ setGood(goo)}
-  return (
-  <button onClick={clicked}>{text}</button>
+  </div>
   )
-}*/
-function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const incrementGood = ()=>{
-    setGood(good + 1)
   }
-  const incrementNeutral = ()=>{
-    setNeutral(neutral + 1)}
-  const incrementBad = ()=>{
-    setBad(bad + 1)}
+  return (
+    <div>
+      <h1> statistics </h1>
+        <table>
+        <tbody>
+          <tr>
+            <td><StatisticLine text="good" value={good}/></td>
+          </tr>
+          <tr>
+            <td><StatisticLine text="neutral" value={neutral}/></td>
+            </tr>
+            <tr>
+            <td><StatisticLine text="bad" value={bad}/></td>
+            </tr>
+            <tr>
+            <td><StatisticLine text="all" value={all}/></td>
+            </tr>
+            <tr>
+            <td><StatisticLine text="average" value={average}/></td>
+            </tr>
+            <tr>
+            <td><StatisticLine text="positive" value={positive}/></td>
+          </tr>
+          </tbody>
+        </table>
+    </div>
+  );
+};
+
+
+const StatisticLine =  ({text, value})=>{ return (<span> {text}: {value} </span>)}
+
+
+const Button = ({text, clicked})=> <button onClick={clicked}>{text}</button>
+
+const App = () =>{
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementGood = () => {
+    setGood(good + 1);
+  };
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const incrementBad = () => {
+    setBad(bad + 1);
+  };
 
   return (
-   <>
-   <Header/>
-
-   <button onClick={incrementGood}>good</button> <button onClick={incrementNeutral}>neutral</button> <button onClick={incrementBad}>bad</button>
-   <Statistics good={good}  neutral={neutral}  bad={bad}/>
-   </>
+    <>
+      <Header/>
+      <Button text="good" clicked={incrementGood}/>
+      <Button text="neutral" clicked={incrementNeutral}/>
+      <Button text="bad" clicked={incrementBad}/>
+      <Statistics good={good} neutral={neutral}  bad={bad} />
+    </>
   );
 }
 
